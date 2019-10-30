c2e50936372c13009dfdec530edd14fb01c9ba74
Saad Khan <saadnyc@Saads-Air.fios-router.home>
2019-10-30 13:00:03 -0400

    This is a test

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -1,33 +1,18 @@
-//  Created by Frank M. Carrano and Timothy M. Henry.
-//  Copyright (c) 2017 Pearson Education, Hoboken, New Jersey.
-//
-//  Modified by Tiziana Ligorio for Hunter College CSCI 235
-//  modified position s.t. 0 <= position < item_count_
-//  some style modification, mainly variable names
-
-/** ADT list: Singly linked list implementation.
-
- Implementation file for the class LinkedList.
- @file LinkedList.cpp */
-
-#include "LinkedList.hpp"  // head_ptrer file
+#include "LinkedList.hpp" // Header file
 #include <cassert>
 
-// constructor
 template<class T>
 LinkedList<T>::LinkedList() : head_ptr_(nullptr), item_count_(0)
 {
-}  // end default constructor
+} // end default constructor
 
-
-// copy constructor
 template<class T>
 LinkedList<T>::LinkedList(const LinkedList<T>& a_list) : item_count_(a_list.item_count_)
 {
-   Node<T>* orig_chain_pointer = a_list.head_ptr_;  // Points to nodes in original chain
+   Node<T>* orig_chain_pointer = a_list.head_ptr_; // Points to nodes in original chain
 
    if (orig_chain_pointer == nullptr)
-      head_ptr_ = nullptr;  // Original list is empty
+      head_ptr_ = nullptr; // Original list is empty
    else
    {
       // Copy first node
@@ -53,56 +38,41 @@ LinkedList<T>::LinkedList(const LinkedList<T>& a_list) : item_count_(a_list.item
 
          // Advance original-chain pointer
          orig_chain_pointer = orig_chain_pointer->getNext();
-      }  // end while
+      } // end while
 
       new_chain_ptr->setNext(nullptr);              // Flag end of chain
-   }  // end if
-}  // end copy constructor
+   } // end if
+} // end copy constructor
 
-
-// destructor
 template<class T>
 LinkedList<T>::~LinkedList()
 {
    clear();
-}  // end destructor
+} // end destructor
 
-
-
-/**@return true if list is empty - item_count_ == 0 */
 template<class T>
 bool LinkedList<T>::isEmpty() const
 {
    return item_count_ == 0;
-}  // end isEmpty
+} // end isEmpty
 
-
-/**@return the number of items in the list - item_count_ */
 template<class T>
 int LinkedList<T>::getLength() const
 {
    return item_count_;
-}  // end getLength
+} // end getLength
 
-
-
-/**
- @pre list positions follow traditional indexing from 0 to item_count_ -1
- @param position indicating point of insertion
- @param new_entry to be inserted in list
- @post new_entry is added at position in list (the node previously at that position is now at position+1)
- @return true if valid position (0 <= position <= item_count_) */
 template<class T>
-bool LinkedList<T>::insert(int positions, const T& new_entry)
+bool LinkedList<T>::insert(int new_position, const T& new_entry)
 {
-   bool able_to_insert = (positions >= 0) && (positions <= item_count_ );
+   bool able_to_insert = (new_position >= 1) && (new_position <= item_count_ + 1);
    if (able_to_insert)
    {
       // Create a new node containing the new entry
       Node<T>* new_node_ptr = new Node<T>(new_entry);
 
       // Attach new node to chain
-      if (positions == 0)
+      if (new_position == 1)
       {
          // Insert new node at beginning of chain
          new_node_ptr->setNext(head_ptr_);
@@ -111,34 +81,27 @@ bool LinkedList<T>::insert(int positions, const T& new_entry)
       else
       {
          // Find node that will be before new node
-         Node<T>* prev_ptr = getNodeAt(positions - 1);
+         Node<T>* prev_ptr = getNodeAt(new_position - 1);
 
          // Insert new node after node to which prev_ptr points
          new_node_ptr->setNext(prev_ptr->getNext());
          prev_ptr->setNext(new_node_ptr);
-      }  // end if
+      } // end if
 
-      item_count_++;  // Increase count of entries
-   }  // end if
+      item_count_++; // Increase count of entries
+   } // end if
 
    return able_to_insert;
-}  // end insert
+} // end insert
 
-
-
-/**
- @pre list positions follow traditional indexing from 0 to item_count_ -1
- @param position indicating point of deletion
- @post node at position is deleted, if any. List order is retains
- @return true if there is a node at position to be deleted, false otherwise */
 template<class T>
 bool LinkedList<T>::remove(int position)
 {
-   bool able_to_remove = (position >= 0) && (position < item_count_);
+   bool able_to_remove = (position >= 1) && (position <= item_count_);
    if (able_to_remove)
    {
       Node<T>* cur_ptr = nullptr;
-      if (position == 0)
+      if (position == 1)
       {
          // Remove the first node in the chain
          cur_ptr = head_ptr_; // Save pointer to node
@@ -155,114 +118,69 @@ bool LinkedList<T>::remove(int position)
          // Disconnect indicated node from chain by connecting the
          // prior node with the one after
          prev_ptr->setNext(cur_ptr->getNext());
-      }  // end if
+      } // end if
 
       // Return node to system
       cur_ptr->setNext(nullptr);
       delete cur_ptr;
       cur_ptr = nullptr;
 
-      item_count_--;  // Decrease count of entries
-   }  // end if
+      item_count_--; // Decrease count of entries
+   } // end if
 
    return able_to_remove;
-}  // end remove
+} // end remove
 
-
-
-/**@post the list is empty and item_count_ == 0*/
 template<class T>
 void LinkedList<T>::clear()
 {
    while (!isEmpty())
-      remove(0);
-}  // end clear
+      remove(1);
+} // end clear
 
-
-
-/**
- @pre list positions follow traditional indexing from 0 to item_count_ -1
- @param position indicating the position of the data to be retrieved
- @return data item found at position. If position is not a valid position < item_count_
- throws  PrecondViolatedExcep */
 template<class T>
 T LinkedList<T>::getEntry(int position) const
 {
-    // Enforce precondition
-    bool ableToGet = (position >= 0) && (position < item_count_);
-    if (ableToGet)
-    {
-        Node<T>* nodePtr = getNodeAt(position);
-        return nodePtr->getItem();
-    }
+    T dummy;
+   // Check precondition
+   bool able_to_get = (position >= 1) && (position <= item_count_);
+   if (able_to_get)
+   {
+      Node<T>* nodePtr = getNodeAt(position);
+      return nodePtr->getItem();
+   }
     else
-    {
-        std::string message = "getEntry() called with an empty list or ";
-        message  = message + "invalid position.";
-        throw(PrecondViolatedExcep(message));
-    }  // end if
-}  // end getEntry
+        return dummy; //PROBLEM!!!! may return uninitialized object - will fix later with Exception Handling
 
+} // end getEntry
+
+/************* PROJECT-SPECIFIC PUBLIC METHODS ************/
+
+// A wrapper to a recursive method that inverts the contents of the list
+// @post the contents of the list are inverted such that
+//      the item previously at position 1 is at position item_count_,
+//      the item previously at position 2 is at position item_count_-1 ...
+//      the item previously at position âŒŠitem_count/2âŒ‹ is at position âŒˆitem_count_/2âŒ‰
 template<class T>
 void LinkedList<T>::invert() {
-  if (head_ptr_ == NULL || head_ptr_->next == NULL) 
-        return head_ptr_;
+invertRest(head_ptr_);
 }
 
-template<class T>
-void LinkedList<T>::rotate(Node **head_ptr_ref, int k){
-    if (k == 0)  
-    return;  
-  
- 
-    Node* current = *head_ptr_ref;  
-  
-    int count = 1;  
-    while (count < k && current != NULL)  
-    {  
-        current = current->next;  
-        count++;  
-    }  
-  
- 
-    if (current == NULL)  
-        return;  
-  
+/************* PRIVATE ************/
 
-    Node *kthNode = current;  
-
-    while (current->next != NULL)  
-        current = current->next;  
-  
-    current->next = *head_ptr_ref;  
-
-    *head_ptr_ref = kthNode->next;  
-
-    kthNode->next = NULL;  
-}  
-
-
-
-
-/************* PRIVATE METHODS ************/
-
-
-// Locates a specified node in this linked list.
-// @pre list positions follow traditional indexing from 0 to item_count_ -1
-// @param position the index of the desired node
-//       0 <= position < item_count_
-// @return  A pointer to the node at the given position or nullptr if position is >= item_count_
 template<class T>
 Node<T>* LinkedList<T>::getNodeAt(int position) const
 {
-    // Count from the beginning of the chain
-    Node<T>* cur_ptr = head_ptr_;
-    for (int skip = 0; skip < position; skip++)
-        cur_ptr = cur_ptr->getNext();
+   // Debugging check of precondition
+   assert( (position >= 1) && (position <= item_count_) );
 
-    return cur_ptr;
-}  // end getNodeAt
+   // Count from the beginning of the chain
+   Node<T>* cur_ptr = head_ptr_;
+   for (int skip = 1; skip < position; skip++)
+      cur_ptr = cur_ptr->getNext();
 
+   return cur_ptr;
+} // end getNodeAt
 
 template<class T>
 void LinkedList<T>::invertRest(Node<T>* current_first_ptr) {
@@ -281,6 +199,4 @@ Prev->setNext(current_first_ptr);
 current_first_ptr->setNext(nullptr);
 }
 
-
-//  End of implementation file.
-
+// End of implementation file.
